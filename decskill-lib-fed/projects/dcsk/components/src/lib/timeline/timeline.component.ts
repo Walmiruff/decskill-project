cc0bf5cf79e56a77d381b2b7f1bf483715d8249d
M269866 <walmircastro@sisconsultoria.com.br>
2023-11-06 12:06:23 -0300

    ajustes imports sem dependecias

--- decskill-lib-fed/projects/dcsk/components/src/lib/timeline/timeline.component.ts
+++ decskill-lib-fed/projects/dcsk/components/src/lib/timeline/timeline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { IPost } from '@dcsk/store';
+import { IPost } from '../models/post.model';
 
 @Component({
   selector: 'dcsk-timeline',
